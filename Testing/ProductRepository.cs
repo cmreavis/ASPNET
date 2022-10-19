8c5e1e0d1bf38389d516e0900ee33c98e9974a4f
cmreavis97 <cmreavis97@gmail.com>
2022-10-19 16:55:33 -0400

    create product working

--- Testing/ProductRepository.cs
+++ Testing/ProductRepository.cs
@@ -30,5 +30,24 @@ namespace Testing
             _connection.Execute("UPDATE products SET Name = @name, Price = @price WHERE ProductID = @id", 
                 new { name = product.Name, price = product.Price, id = product.ProductID });
         }
+        public void InsertProduct(Product productToInsert)
+        {
+            _connection.Execute("INSERT INTO products (NAME, PRICE, CATEGORYID) VALUES (@name, @price, @categoryID);",
+                new { name = productToInsert.Name, price = productToInsert.Price, categoryID = productToInsert.CategoryID });
+        }
+
+        public Product AssignCategory()
+        {
+            var categoryList = GetAllCategories();
+            var product = new Product();
+            product.Categories = categoryList;
+            return product;
+        }
+
+        public IEnumerable<Category> GetAllCategories()
+        {
+            return _connection.Query<Category>("SELECT * FROM categories;");
+        }
+
     }
 }
