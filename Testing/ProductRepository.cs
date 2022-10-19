d958885f35add2d2f090aa384fbcf374af13eb21
cmreavis97 <cmreavis97@gmail.com>
2022-10-19 15:59:50 -0400

    products page added, functional

---
+++ Testing/ProductRepository.cs
@@ -0,0 +1,24 @@
+﻿using Dapper;
+using System.Collections;
+using System.Collections.Generic;
+using System.Data;
+using Testing.Models;
+
+namespace Testing
+{
+    public class ProductRepository : IProductRepository
+    {
+        private readonly IDbConnection _connection;
+
+        public ProductRepository(IDbConnection connection)
+        {
+            _connection = connection;
+        }
+
+        public IEnumerable<Product> GetAllProducts()
+        {
+            return _connection.Query<Product>("SELECT * FROM products;");
+        }
+
+    }
+}
